811c3b7ec57dbed780a13837063154b9a432fe57
tommyhosmer <130514519+tommyhosmer@users.noreply.github.com>
2024-12-28 07:59:44 +0000

    Deploying to gh-pages from @ tommyhosmer/tommyhosmer.github.io@ff6a1c42196cc9c63111d2aa3a89fb61dcf9a6fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "My CV. Please direct any questions to tommyhosmer@berkeley.edu.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -431,18 +431,7 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+    },{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
