991002932f5169ae242c779da40b4d87f983095a
tommyhosmer <130514519+tommyhosmer@users.noreply.github.com>
2024-12-29 08:10:49 +0000

    Deploying to gh-pages from @ tommyhosmer/tommyhosmer.github.io@4ef7f756dc6c7b26c1d2d081893cc9aa9b4e6420 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A mix of class and personal projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
